364eff0c9883dd305e2791eb5bf87973d3c7462c
robrien-bcgov <richard.obrien@gov.bc.ca>
2024-07-17 13:57:21 -0700

    build project

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'c62f4cf4887d19aef4e4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '417f11d605af86e0624f');
